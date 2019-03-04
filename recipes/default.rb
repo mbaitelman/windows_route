0076ef4e16848331922b8639a02df48e07c5d901
Mendy Baitelman <mendy@baitelman.com>
2019-03-04 12:55:04 -0800

    Working but still work in progress

--- recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook:: windows_route
-# Recipe:: default
-#
-# Copyright:: 2019, The Authors, All Rights Reserved.
--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,10 +1,36 @@
 
-windows_route '10.0.1.10/32' do
+windows_route '10.0.1.11/32' do
     gateway '0.0.0.0'
     device 'eth0'
 end
 
-windows_route '10.0.1.10/32' do
+windows_route '10.3.1.12/32' do
+    gateway '0.0.0.1'
+    device 'eth0'
+end
+
+windows_route '10.3.1.12/32' do
+    gateway '0.0.0.2'
+    device 'eth0'
+end
+
+windows_route '10.2.1.10/32' do
+    gateway '0.0.0.0'
+    action :add
+end
+
+windows_route '10.2.1.10/32' do
     gateway '0.0.0.0'
     action :delete
+end
+
+windows_route '10.2.1.10/32' do
+    gateway '0.0.0.0'
+    action :delete
+end
+
+windows_route '11.0.0.0' do 
+    netmask '255.255.0.0'
+    gateway '192.168.51.221'
+    action :add
 end
