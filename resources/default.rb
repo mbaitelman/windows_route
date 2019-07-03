63df571f54e34b6fc36f75acbc8b130810017c5b
Mendy Baitelman <mendy@baitelman.com>
2019-07-03 12:10:25 -0700

    Cookstyle magic

--- resources/default.rb
+++ resources/default.rb
@@ -5,53 +5,53 @@
 # default_action :add
 # allowed_actions :add, :delete
 
-description "Use the route resource to manage the system routing table in a Windows environment."
+description 'Use the route resource to manage the system routing table in a Windows environment.'
 
 property :target, String,
-          description: "The IP address of the target route.",
+          description: 'The IP address of the target route.',
           identity: true, name_property: true
 
 property :comment, [String, nil],
-          description: "Add a comment for the route. Not supported in windows_route."
+          description: 'Add a comment for the route. Not supported in windows_route.'
 
 property :metric, [Integer, nil],
-          description: "The route metric value."
+          description: 'The route metric value.'
 
 property :netmask, [String, nil],
-          description: "The decimal representation of the network mask. For example: 255.255.255.0."
+          description: 'The decimal representation of the network mask. For example: 255.255.255.0.'
 
 property :gateway, [String, nil],
-          description: "The gateway for the route."
+          description: 'The gateway for the route.'
 
 property :device, [String, nil],
-          description: "The network interface to which the route applies.", # Has a partial default in the action to get the first interface.
-          default: nil 
+          description: 'The network interface to which the route applies.', # Has a partial default in the action to get the first interface.
+          default: nil
 
 property :route_type, [Symbol, String, nil],
-          description: "",
-          equal_to: [:host, :net], 
+          description: '',
+          equal_to: [:host, :net],
           default: nil
 
 property :persistent, [TrueClass, FalseClass],
-          default: true 
+          default: true
 
 alias_method :interface_alias, :device
 
-action :add do 
+action :add do
   description ''
   validate_attributes
 
-  code_script = "New-NetRoute -AddressFamily IPv4 "
+  code_script = 'New-NetRoute -AddressFamily IPv4 '
   code_script << " -DestinationPrefix #{new_resource.target}"
   code_script << " -RouteMetric #{new_resource.metric}" if new_resource.metric
   code_script << " -NextHop #{new_resource.gateway}" if new_resource.gateway
-  if new_resource.device.nil?
-    ifalias = powershell_out!('((Get-NetIPInterface -AddressFamily IPv4)[0].ifAlias)').stdout.strip
-  else 
-    ifalias = new_resource.device
-  end
+  ifalias = if new_resource.device.nil?
+              powershell_out!('((Get-NetIPInterface -AddressFamily IPv4)[0].ifAlias)').stdout.strip
+            else
+              new_resource.device
+            end
   code_script << " -InterfaceAlias '#{ifalias}'"
-  code_script << " -PolicyStore ActiveStore" if !new_resource.persistent
+  code_script << ' -PolicyStore ActiveStore' unless new_resource.persistent
 
   guard_script = create_guard_script
 
@@ -61,7 +61,7 @@ action :add do
   end
 end
 
-action :delete do 
+action :delete do
   description ''
   validate_attributes
   code_script = "Remove-NetRoute -DestinationPrefix #{new_resource.target} -Confirm:$false"
@@ -74,30 +74,28 @@ action :delete do
 end
 
 action_class do
-  def validate_attributes 
+  def validate_attributes
     ipaddr = IPAddr.new new_resource.target
-    if !ipaddr.ipv4?() #Checks IPv4
-      raise 'IP is not IPv4 compliant'
-    end
-    if !new_resource.target.include? '/' # Checks if valid CIDR # TODO fix cidr check
+    raise 'IP is not IPv4 compliant' unless ipaddr.ipv4?() # Checks IPv4
+    unless new_resource.target.include? '/' # Checks if valid CIDR # TODO fix cidr check
       raise "IP #{new_resource.target} is not a valid CIDR map"
     end
     # Check attributes that exist in the linux version
-    if !new_resource.comment.nil? # Checks :comment as not supported
+    unless new_resource.comment.nil? # Checks :comment as not supported
       warn 'The :comment attribute is not supported in the windows_route resource'
     end
-    if !new_resource.netmask.nil? # Checks :netmask as not supported # TODO check if it can be supported
+    unless new_resource.netmask.nil? # Checks :netmask as not supported # TODO check if it can be supported
       warn 'The :netmask attribute is not supported in the windows_route resource'
-    end    
-    if !new_resource.route_type.nil? # Checks :route_type as not supported # TODO check if it can be supported
+    end
+    unless new_resource.route_type.nil? # Checks :route_type as not supported # TODO check if it can be supported
       warn 'The :route_type attribute is not supported in the windows_route resource'
-    end 
+    end
   end
 
   def create_guard_script # Returns script that will respond with a boolean value of whether or not a route exits for the target
-    script = "[bool](Get-NetRoute -AddressFamily IPv4"
+    script = '[bool](Get-NetRoute -AddressFamily IPv4'
     script << " -DestinationPrefix #{new_resource.target}"
-    script << " -ErrorAction Ignore)"
+    script << ' -ErrorAction Ignore)'
     script
-  end  
+  end
 end
--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,25 +1,25 @@
 
 windows_route '10.0.1.11/32' do
-    gateway '0.0.0.0'
-    interface_alias node['if_name'] 
-    comment 'a comment!'
-    persistent false
+  gateway '0.0.0.0'
+  interface_alias node['if_name']
+  comment 'a comment!'
+  persistent false
 end
 
 windows_route '10.3.1.12/32' do
-   gateway '0.0.0.0'
-   metric 200
-   device node['if_name'] 
+  gateway '0.0.0.0'
+  metric 200
+  device node['if_name']
 end
 
 windows_route '10.3.1.12/32' do
-   gateway '0.0.0.2'
-    device node['if_name'] 
+  gateway '0.0.0.2'
+  device node['if_name']
 end
 
 windows_route '10.2.1.10/32' do
-    gateway '0.0.0.0'
-    action :add
+  gateway '0.0.0.0'
+  action :add
 end
 
 # windows_route '2001:0db8:85a3:0000:0000:8a2e:0370:7334' do
@@ -28,17 +28,17 @@ end
 # end
 
 windows_route '10.2.1.10/32' do
-    gateway '0.0.0.0'
-    action :delete
+  gateway '0.0.0.0'
+  action :delete
 end
 
 windows_route '10.2.1.10/32' do
-    gateway '0.0.0.0'
-    action :delete
+  gateway '0.0.0.0'
+  action :delete
 end
 
-windows_route '10.4.0.1/32' do 
-    netmask '255.255.0.0'
-    gateway '0.0.0.0'
-    action :add
+windows_route '10.4.0.1/32' do
+  netmask '255.255.0.0'
+  gateway '0.0.0.0'
+  action :add
 end
